3ff94ee6cf32137cecf9e43240d62243efc9cd0c
Severin Zumbrunn <severin.zumbrunn@students.unibe.ch>
2015-03-03 12:34:09 +0100

    #DH-73 #time 2h 3m #comment Updated client, updated newtradition method

--- stemmarest/src/main/java/net/stemmaweb/rest/Rest.java
+++ stemmarest/src/main/java/net/stemmaweb/rest/Rest.java
@@ -1,19 +1,28 @@
 package net.stemmaweb.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 /**
  * Root resource (exposed at "rest" path)
@@ -23,6 +32,13 @@ public class Rest {
 	
 	public static final String DB_PATH = "database"; // this is the local path to StemmaServer/database
 
+	@GET 
+    @Produces("text/plain")
+    public String getIt() {
+		System.out.println("Hello, World!");
+        return "hello from rest.java!";
+    }
+	
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -31,18 +47,30 @@ public class Rest {
      */
     
     @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    @Consumes("application/x-www-form-urlencoded")
     @Path("/newtradition")
-    public String create(MultivaluedMap<String, String> formParams) {
+    public String create(@FormDataParam("name") String name,
+    					@FormDataParam("file") InputStream uploadedInputStream,
+    					@FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException {
        
-    	
+    	if(uploadedInputStream!=null)
+				System.out.println(uploadedInputStream.available());
+
+    	if(fileDetail!=null)
+    	{
+    		System.out.println("Test ok " + name + "" + fileDetail.getFileName());
+    	}
+    	else
+    	{
+    		System.out.println("error filedetail is empty");
+    	}
     	GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
     	GraphDatabaseService db= dbFactory.newEmbeddedDatabase(DB_PATH);
 
     	try (Transaction tx = db.beginTx()) {
     		Node tradition = db.createNode(Nodes.TRADITION);	
-    		tradition.setProperty("name", formParams.get("traditionname").get(0));
+    		tradition.setProperty("name", name);
     		
     		tx.success();
     	}
@@ -57,5 +85,6 @@ public class Rest {
     	}
     	
     	return "{\"Status\": \"OK\"}";
+    	//return Response.status(200).entity("Return message").build();
     }
 }
