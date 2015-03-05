4d5c3e0f4608b1c397d71813d01b7d93686122ac
idoger <ido.gerschoni@students.unibe.ch>
2015-03-05 17:12:33 +0100

    #DH-33 #time 2h #comment Work on getting a witness from db

--- stemmarest/src/main/java/net/stemmaweb/rest/Witness.java
+++ stemmarest/src/main/java/net/stemmaweb/rest/Witness.java
@@ -1,38 +1,85 @@
 package net.stemmaweb.rest;
 
+import java.util.Iterator;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.neo4j.cypher.ExecutionEngine;
+import org.neo4j.cypher.ExecutionResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.impl.util.StringLogger;
+
+import Exeptions.DataBaseExeption;
 
 /**
  * 
- * @author jakobschaerer
+ * @author jakobschaerer/ido
  *
- */
+ **/
+
 @Path("/witness")
 public class Witness {
-    
-	@GET 
-    @Produces("text/plain")
-    public String getIt() {
-        return "The Witnesses";
-    }
-	
+	public static final String DB_PATH = "database";
+
+	/*
+	 * gets the requested witness as a string
+	 */
+
+	@GET
+	@Path("{textId}")
+	@Produces("text/plain")
+	public String getWitnssAsPlainText(@PathParam("textId") String textId)
+			throws DataBaseExeption {
+		Node node;
+		String witnessAsText = "";
+		GraphDatabaseService db = new GraphDatabaseFactory()
+				.newEmbeddedDatabase(DB_PATH);
+		ExecutionEngine engine = new ExecutionEngine(db, StringLogger.SYSTEM);
+
+		ExecutionResult result;
+		try (Transaction tx = db.beginTx()) {
+
+			result = engine.execute("match (n {leximes: '" + textId
+					+ "'}) return n");
+			Iterator<Node> nodes = (Iterator<Node>) result.columnAs("n");
+			if (nodes.hasNext())
+				throw new DataBaseExeption("more that one node with same Id");
+			node = nodes.next();
+			while (nodes.hasNext()) { // TODO not correct! only temporary!
+				result = engine.execute("match (" + node
+						+ "-[NORMAL]-(b)) return b"); // not sure if this will
+														// work
+				nodes = (Iterator<Node>) result.columnAs("b");
+				node = nodes.next();
+				if (nodes.hasNext())
+					throw new DataBaseExeption(
+							"more that one NORMAL relationship to a single node");
+
+				witnessAsText += "" + node.getProperty("word");  // TODO check property name
+			}
+		}
+		return witnessAsText;
+
+	}
+
 	/**
 	 * 
 	 * @param textId
-	 * @return a witness as //TODO what 
+	 * @return a witness as a list of readings
 	 */
-	
-	@GET
+
 	@Path("{textId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getReadings(@PathParam("textId") String textId){
+	public String getWitnessAsReadings(@PathParam("textId") String textId) {
+
 		return textId;
 	}
 
-	
 }
