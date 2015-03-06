ee056b2c877ffe5ff7a8a3ddcd01186438d62108
Severin Zumbrunn <severin.zumbrunn@students.unibe.ch>
2015-03-06 20:46:53 +0100

    #DH-78 #time 30m #comment Added initialize method for server. Initializes DB now with THE Root node

---
+++ stemmarest/src/main/java/net/stemmaweb/rest/ApplicationConfig.java
@@ -0,0 +1,54 @@
+package net.stemmaweb.rest;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+
+public class ApplicationConfig extends Application {
+	
+	public static final String DB_PATH = "database"; // this is the local path to StemmaServer/database
+	static boolean init = false;
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> s = new HashSet<Class<?>>();
+        s.add(MyResource.class);
+        s.add(Witness.class);
+        s.add(Rest.class);
+        
+        if(!init)
+        	initializeApp();
+        
+        return s;
+    }
+    
+    public void initializeApp()
+    {
+    	init = true;
+    	
+    	GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
+    	GraphDatabaseService db= dbFactory.newEmbeddedDatabase(DB_PATH);
+    	
+    	ExecutionEngine engine = new ExecutionEngine(db);
+    	try(Transaction tx = db.beginTx())
+    	{
+    		ExecutionResult result = engine.execute("match (n:ROOT) return n");
+    		Iterator<Node> nodes = result.columnAs("n");
+    		if(!nodes.hasNext())
+    		{
+    			Node node = db.createNode(Nodes.ROOT);
+    		}
+    		tx.success();
+    	}
+    }
+}
