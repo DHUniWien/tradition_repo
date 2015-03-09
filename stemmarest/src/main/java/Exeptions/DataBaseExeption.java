d6b8e800ee4a91b3c65c5752e1ae38b67a8fba7a
idoger <ido.gerschoni@students.unibe.ch>
2015-03-09 16:11:59 +0100

    #DH-33#time 1h#comment work on witness - change quarry to match data base scheme

--- stemmarest/src/main/java/Exeptions/DataBaseExeption.java
+++
@@ -1,9 +0,0 @@
-package Exeptions;
-
-public class DataBaseExeption extends RuntimeException {
-
-	public DataBaseExeption(String string) {
-		super(string);
-	}
-
-}
--- stemmarest/src/main/java/net/stemmaweb/rest/Witness.java
+++ stemmarest/src/main/java/net/stemmaweb/rest/Witness.java
@@ -14,16 +14,15 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.kernel.impl.util.StringLogger;
 
-import Exeptions.DataBaseExeption;
+import Exceptions.DataBaseException;
 
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 
 /**
  * 
- * @author jakobschaerer/ido
+ * @author jakob/ido
  *
  **/
 
@@ -36,46 +35,51 @@ public class Witness {
 	 */
 
 	@GET
-	@Path("{textId}")
+	@Path("{textId}, {userId}, {traditionName}")
 	@Produces("text/plain")
-	public String getWitnssAsPlainText(@PathParam("textId") String textId)
-			throws DataBaseExeption {
+	public String getWitnssAsPlainText(@PathParam("userId") String userId,
+			@PathParam("traditionName") String traditionName,
+			@PathParam("textId") String textId) throws DataBaseException {
 		Node node;
 		String witnessAsText = "";
 		GraphDatabaseService db = new GraphDatabaseFactory()
 				.newEmbeddedDatabase(DB_PATH);
-		ExecutionEngine engine = new ExecutionEngine(db, StringLogger.SYSTEM);
+		ExecutionEngine engine = new ExecutionEngine(db);
 
 		ExecutionResult result;
-		String witnessQuary = "match (n {leximes:'" + textId + "'}) return n";
+		String witnessQuary = "match s,(user:USER {id:'" + userId
+				+ "'})-[r]->(tradition:TRADITION {name:'" + traditionName
+				+ "'}), (tradition)-[r]->(s:WORD {id:'" + traditionName
+				+ "__START__'}), p=(s)--[r]-->(b)--[r]-->(c)"
+						+ " where r.id= '"+ textId + "' AND c.id= '"+traditionName+"__END__' return nodes(p)";
 
 		try (Transaction tx = db.beginTx()) {
 
 			result = engine.execute(witnessQuary);
-			Iterator<Node> nodes = result.columnAs("n");
+			Iterator<Node> nodes = result.columnAs("s");
 			if (nodes.hasNext())
-				throw new DataBaseExeption("more that one node with same Id");
+				throw new DataBaseException(
+						"a word with more than one relationship with the same id");
 			if (!nodes.hasNext())
-				throw new DataBaseExeption(
+				throw new DataBaseException(
 						"such witness does not exist in the data base");
 			else
 				node = nodes.next();
-			String nextWordQuary = "match (n {Id:'" + node.getId()
-					+ "'})-[:NORMAL]-(b)) return b"; // not sure if this will
-														// work
+			
+			
+			/*String nextWordQuary = "match (n)-[r {id:'" + node.getId()
+					+ "'}]-(b)) return b";
 
 			while (nodes.hasNext()) { // TODO not correct! only temporary!
 				result = engine.execute(nextWordQuary);
 				nodes = result.columnAs("b");
 				node = nodes.next();
 				if (nodes.hasNext())
-					throw new DataBaseExeption(
-							"more that one NORMAL relationship to a single node");
+					throw new DataBaseException(
+							"more than one NORMAL relationship to a single node");
 
-				witnessAsText += " " + node.getProperty("word"); // TODO check
-																	// property
-																	// name
-			}
+				witnessAsText += " " + node.getProperty("text");
+			}*/
 		}
 		return witnessAsText;
 	}
@@ -92,5 +96,4 @@ public class Witness {
 
 		return textId;
 	}
-
 }
