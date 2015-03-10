c89948a09b878fddfab7bd72cf01d436919f7242
idoger <ido.gerschoni@students.unibe.ch>
2015-03-10 10:48:20 +0100

    #DH-33#comment push again

--- stemmarest/src/main/java/net/stemmaweb/rest/Witness.java
+++ stemmarest/src/main/java/net/stemmaweb/rest/Witness.java
@@ -8,12 +8,11 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-//import org.neo4j.cypher.ExecutionEngine;
-//import org.neo4j.cypher.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.helpers.collection.IteratorUtil;
 
 import Exceptions.DataBaseException;
 
@@ -29,6 +28,8 @@ import org.neo4j.cypher.javacompat.ExecutionResult;
 @Path("/witness")
 public class Witness {
 	public static final String DB_PATH = "database";
+	private GraphDatabaseService db = new GraphDatabaseFactory()
+			.newEmbeddedDatabase(DB_PATH);
 
 	/*
 	 * gets the requested witness as a string
@@ -40,46 +41,43 @@ public class Witness {
 	public String getWitnssAsPlainText(@PathParam("userId") String userId,
 			@PathParam("traditionName") String traditionName,
 			@PathParam("textId") String textId) throws DataBaseException {
-		Node node;
 		String witnessAsText = "";
-		GraphDatabaseService db = new GraphDatabaseFactory()
-				.newEmbeddedDatabase(DB_PATH);
+
 		ExecutionEngine engine = new ExecutionEngine(db);
 
 		ExecutionResult result;
-		String witnessQuary = "match s,(user:USER {id:'" + userId
-				+ "'})-[r]->(tradition:TRADITION {name:'" + traditionName
-				+ "'}), (tradition)-[r]->(s:WORD {id:'" + traditionName
-				+ "__START__'}), p=(s)--[r]-->(b)--[r]-->(c)"
-						+ " where r.id= '"+ textId + "' AND c.id= '"+traditionName+"__END__' return nodes(p)";
+		String witnessQuary = "match (user:USER {id:'" + userId
+				+ "'})-[r1]->(tradition:TRADITION {name:'" + traditionName
+				+ "'}), (tradition)-[r2]->(s:WORD {id:'" + traditionName
+				+ "__START__'}), p=(s)<--(b)<--(c), (s)<-[r]-(b)"
+				+ " where r.id= '" + textId + "' AND c.id= '" + traditionName
+				+ "__END__' return extract(n IN nodes(p)| n.text) AS extracted";
 
 		try (Transaction tx = db.beginTx()) {
 
 			result = engine.execute(witnessQuary);
-			Iterator<Node> nodes = result.columnAs("s");
-			if (nodes.hasNext())
-				throw new DataBaseException(
-						"a word with more than one relationship with the same id");
-			if (!nodes.hasNext())
-				throw new DataBaseException(
-						"such witness does not exist in the data base");
-			else
-				node = nodes.next();
-			
-			
-			/*String nextWordQuary = "match (n)-[r {id:'" + node.getId()
-					+ "'}]-(b)) return b";
+			Iterator<String> texts = result.columnAs("n.text");
 
-			while (nodes.hasNext()) { // TODO not correct! only temporary!
-				result = engine.execute(nextWordQuary);
-				nodes = result.columnAs("b");
-				node = nodes.next();
-				if (nodes.hasNext())
-					throw new DataBaseException(
-							"more than one NORMAL relationship to a single node");
+			if (!texts.hasNext())
+				throw new DataBaseException(findPathProblem(userId,
+						traditionName, textId));
+			else {
+				for (String text : IteratorUtil.asIterable(texts))
+					witnessAsText = " " + text;
+			}
 
-				witnessAsText += " " + node.getProperty("text");
-			}*/
+			/*
+			 * String nextWordQuary = "match (n)-[r {id:'" + node.getId() +
+			 * "'}]-(b)) return b";
+			 * 
+			 * while (nodes.hasNext()) { // TODO not correct! only temporary!
+			 * result = engine.execute(nextWordQuary); nodes =
+			 * result.columnAs("b"); node = nodes.next(); if (nodes.hasNext())
+			 * throw new DataBaseException(
+			 * "more than one NORMAL relationship to a single node");
+			 * 
+			 * witnessAsText += " " + node.getProperty("text"); }
+			 */
 		}
 		return witnessAsText;
 	}
@@ -95,5 +93,56 @@ public class Witness {
 	public String getWitnessAsReadings(@PathParam("textId") String textId) {
 
 		return textId;
+	}
+
+	/**
+	 * will find the missing link in the data base path in case of an empty
+	 * result in the witness search
+	 * 
+	 * @param userId
+	 * @param traditionName
+	 * @param textId
+	 * @return the text to be displayed in the exception
+	 */
+	private String findPathProblem(String userId, String traditionName,
+			String textId) {
+		String exceptionString = "";
+		GraphDatabaseService db = new GraphDatabaseFactory()
+				.newEmbeddedDatabase(DB_PATH);
+		ExecutionEngine engine = new ExecutionEngine(db);
+		try (Transaction tx = db.beginTx()) {
+
+			ExecutionResult userResult = engine.execute("match (n:USER {id:'"
+					+ userId + "'}) return n");
+			Iterator<Node> users = userResult.columnAs("n");
+			if (!users.hasNext())
+				exceptionString = "such user does not exist in the system";
+			else {
+				ExecutionResult traditionResult = engine
+						.execute("match (n:TRADITION {name:'" + traditionName
+								+ "'}) retun n");
+				Iterator<Node> traditions = traditionResult.columnAs("n");
+
+				if (!traditions.hasNext())
+					exceptionString = "such trsdition does not exist in the system";
+				else {
+					ExecutionResult witnessResult = engine
+							.execute("match (n:TRADITION {id:'" + traditionName
+									+ "__START__'}) retun n");
+					Iterator<Node> witnesses = witnessResult.columnAs("n");
+
+					if (!witnesses.hasNext())
+						exceptionString = "such wtiness does not exist in the system";
+					else
+						exceptionString = "there is some unknown problem with the data path";
+				}
+			}
+		}
+
+		return exceptionString;
+	}
+
+	public void setDb(GraphDatabaseService graphDb) {
+		db = graphDb;
 	}
 }
--- stemmarest/src/test/java/net/stemmaweb/stemmaserver/GetWitnessTest.java
+++ stemmarest/src/test/java/net/stemmaweb/stemmaserver/GetWitnessTest.java
@@ -1,30 +1,36 @@
 package net.stemmaweb.stemmaserver;
 
 import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
 import net.stemmaweb.rest.Witness;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.test.TestGraphDatabaseFactory;
-
 import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
 
 public class GetWitnessTest {
 
-	Witness wintess;
-	GraphDatabaseService graphDb;
+	private Witness wintess;
+	private GraphDatabaseService graphDb;
 
 	@Before
 	public void prepareTestDatabase() {
 		graphDb = new TestGraphDatabaseFactory().newImpermanentDatabase();
-		ExecutionEngine engine = new ExecutionEngine(graphDb);
+		ExecutionEngine beforeEngine = new ExecutionEngine(graphDb);
 		String createWitness = "create (testUser:USER {id:'testUserId'}),"
 				+ " (testTradition:TRADITION {name:'testTraditionName'}),"
 				+ " (witnessStart:WORD {name:'testTraditionName__STRAT__'}),"
-				+ " (word1:WORD {text:'this'})," + " (word2:WORD {text:'is'}),"
+				+ " (word1:WORD {text:'this'}),"
+				+ " (word2:WORD {text:'is'}),"
 				+ " (word3:WORD {text:'a'}),"
 				+ " (word4:WORD {text:'witness'}),"
 				+ " (word5:WORD {text:'test'}),"
@@ -36,8 +42,22 @@ public class GetWitnessTest {
 				+ " (word3)-[:TEST_REALTIONSHIP {leximes:'testLexime'}]->(word4),"
 				+ " (word4)-[:TEST_REALTIONSHIP {leximes:'testLexime'}]->(word5);";
 		try (Transaction tx = graphDb.beginTx()) {
-			engine.execute(createWitness);
+			beforeEngine.execute(createWitness);
 			tx.success();
+		}
+	}
+
+	@Test
+	public void testNodeCreation() {
+
+		ExecutionEngine engine = new ExecutionEngine(graphDb);
+		String nodeCreationQuary = "match (n:WORD {text:'this'}) return n";
+
+		try (Transaction tx = graphDb.beginTx()) {
+			ExecutionResult result = engine.execute(nodeCreationQuary);
+			Iterator<Node> nodes = result.columnAs("n");
+			String textProperty = (String) nodes.next().getProperty("text");
+			assertEquals("this", textProperty);
 		}
 	}
 
@@ -45,6 +65,8 @@ public class GetWitnessTest {
 	public void testWintessAsString() {
 
 		wintess = new Witness();
+		wintess.setDb(graphDb);
+		
 		assertEquals("this is a witness test", wintess.getWitnssAsPlainText(
 				"testUserId", "testTraditionName", "testLexime"));
 	}
