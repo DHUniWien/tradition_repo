6c0a033525d8f7e9ba6d52afb2e0c79b294f5461
idoger <ido.gerschoni@students.unibe.ch>
2015-03-19 10:02:36 +0100

    #DH-38#time 1.5h#comment replaced witness unit test with witness integration test. not all tests are green yet

--- stemmarest/src/test/java/net/stemmaweb/stemmaserver/ReadingTest.java
+++ stemmarest/src/test/java/net/stemmaweb/stemmaserver/ReadingTest.java
@@ -4,13 +4,21 @@ import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
 import java.util.Iterator;
+import java.util.List;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import net.stemmaweb.model.TraditionModel;
+import net.stemmaweb.model.ReadingModel;
 import net.stemmaweb.rest.Nodes;
 import net.stemmaweb.rest.Relations;
+import net.stemmaweb.rest.User;
 import net.stemmaweb.rest.Witness;
 import net.stemmaweb.services.DbPathProblemService;
 import net.stemmaweb.services.GraphMLToNeo4JParser;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,34 +32,64 @@ import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.ResourceIterable;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
-@RunWith(MockitoJUnitRunner.class)
-public class ReadingUnitTest {
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.test.framework.JerseyTest;
 
+/**
+ * 
+ * @author Jakob
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ReadingTest {
+	private String tradId;
+	private String[] witIds;
+
+	/*
+	 * Create a Mock object for the dbFactory.
+	 */
 	@Mock
 	protected GraphDatabaseFactory mockDbFactory = new GraphDatabaseFactory();
 
+	/*
+	 * Create a Spy object for dbService.
+	 */
 	@Spy
 	protected GraphDatabaseService mockDbService = new TestGraphDatabaseFactory()
 			.newImpermanentDatabase();
 
+	/*
+	 * The Resource under test. The mockDbFactory will be injected into this
+	 * resource.
+	 */
 	@InjectMocks
 	private GraphMLToNeo4JParser importResource;
 
 	@InjectMocks
 	private Witness witness;
-	
+
 	@InjectMocks
 	private DbPathProblemService problemService;
 
+	/*
+	 * JerseyTest is the test environment to Test api calls it provides a
+	 * grizzly http service
+	 */
+	private JerseyTest jerseyTest;
+
 	@Before
 	public void setUp() throws Exception {
+		// might not work on MAC!!
 		String filename = "src\\TestXMLFiles\\Sapientia.xml";
+
 		/*
 		 * Populate the test database with the root node and a user with id 1
 		 */
@@ -74,35 +112,67 @@ public class ReadingUnitTest {
 			tx.success();
 		}
 
+		/*
+		 * Manipulate the newEmbeddedDatabase method of the mockDbFactory to
+		 * return new TestGraphDatabaseFactory().newImpermanentDatabase()
+		 * instead of dbFactory.newEmbeddedDatabase("database");
+		 */
 		Mockito.when(mockDbFactory.newEmbeddedDatabase(Matchers.anyString()))
 				.thenReturn(mockDbService);
 
+		/*
+		 * Avoid the Databaseservice to shutdown. (Override the shutdown method
+		 * with nothing)
+		 */
 		Mockito.doNothing().when(mockDbService).shutdown();
 
+		/*
+		 * Create a JersyTestServer serving the Resource under test
+		 */
+		jerseyTest = JerseyTestServerFactory.newJerseyTestServer()
+				.addResource(witness).create();
+		jerseyTest.setUp();
+
+		/**
+		 * load a tradition to the test DB
+		 */
 		try {
 			importResource.parseGraphML(filename, "1");
 		} catch (FileNotFoundException f) {
 			// this error should not occur
 			assertTrue(false);
 		}
+		/**
+		 * gets the generated id of the inserted tradition
+		 */
+		try (Transaction tx = mockDbService.beginTx()) {
+			ExecutionResult result = engine
+					.execute("match (u:USER)--(t:TRADITION) return t");
+			Iterator<Node> nodes = result.columnAs("t");
+			assertTrue(nodes.hasNext());
+			tradId = (String) nodes.next().getProperty("id");
+
+			tx.success();
+		}
 	}
 
-	/**
-	 * Import a correct file
-	 */
+	// not working yet!!
 	@Test
 	public void getNextReadingTest() {
-		// not correct yet!
-		assertEquals("next",
-				witness.getNextReadingInWitness("1000", "r1008", "readId"));
-
-		traditionNodeExistsTest();
-		traditionEndNodeExistsTest();
+		
+		ReadingModel readingModel = new ReadingModel();
+		readingModel.setDn1("r1099.2");
+		
+		ReadingModel actualResponse = jerseyTest.resource()
+				.path("witness/reading/next/" + tradId + "/r1099/r1099.1")
+				.get(ReadingModel.class);
+		assertEquals(actualResponse.getDn1(), "r1099.2");
 	}
 
 	/**
 	 * test if the tradition node exists
 	 */
+	@Test
 	public void traditionNodeExistsTest() {
 		try (Transaction tx = mockDbService.beginTx()) {
 			ResourceIterable<Node> tradNodes = mockDbService
@@ -117,6 +187,7 @@ public class ReadingUnitTest {
 	/**
 	 * test if the tradition end node exists
 	 */
+	@Test
 	public void traditionEndNodeExistsTest() {
 		ExecutionEngine engine = new ExecutionEngine(mockDbService);
 
@@ -125,4 +196,16 @@ public class ReadingUnitTest {
 		ResourceIterator<Node> tradNodes = result.columnAs("n");
 		assertTrue(tradNodes.hasNext());
 	}
+
+	/**
+	 * Shut down the jersey server
+	 * 
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		mockDbService.shutdown();
+		jerseyTest.tearDown();
+	}
+
 }
