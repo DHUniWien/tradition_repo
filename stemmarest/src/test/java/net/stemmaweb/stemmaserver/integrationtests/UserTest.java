af15ce21bfe565b814bea234ac8da7aa61ec093d
Jakob Schaerer <jakob.schaerer@students.unibe.ch>
2015-04-02 21:44:02 +0200

    #DH-44 #time 30m #comment working on userTest implementation

--- stemmarest/src/test/java/net/stemmaweb/stemmaserver/integrationtests/UserTest.java
+++ stemmarest/src/test/java/net/stemmaweb/stemmaserver/integrationtests/UserTest.java
@@ -3,17 +3,22 @@ package net.stemmaweb.stemmaserver.integrationtests;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import net.stemmaweb.model.RelationshipModel;
 import net.stemmaweb.model.TraditionModel;
 import net.stemmaweb.model.UserModel;
+import net.stemmaweb.rest.ERelations;
 import net.stemmaweb.rest.Nodes;
 import net.stemmaweb.rest.User;
+import net.stemmaweb.services.GraphMLToNeo4JParser;
 import net.stemmaweb.stemmaserver.JerseyTestServerFactory;
+import net.stemmaweb.stemmaserver.OSDetector;
 
 import org.junit.After;
 import org.junit.Before;
@@ -61,6 +66,9 @@ public class UserTest {
 	/*
 	 * The Resource under test. The mockDbFactory will be injected into this resource.
 	 */
+	@InjectMocks
+	private GraphMLToNeo4JParser importResource;
+	
 	@InjectMocks
 	private User userResource;
 	
@@ -175,6 +183,126 @@ public class UserTest {
 		assertEquals("43",actualResponse.getId());
 		assertEquals("0",actualResponse.getIsAdmin());
 	}
+	
+	/**
+	 * Test if a user is correctly removed with all his subgraphs
+	 */
+	@Test
+	public void deleteUserTest(){
+		
+		/*
+		 * Populate the Database with users and traditions
+		 */
+		String tradId;
+		String filename = "";
+		if (OSDetector.isWin())
+			filename = "src\\TestXMLFiles\\testTradition.xml";
+		else
+			filename = "src/TestXMLFiles/testTradition.xml";
+
+		/*
+		 * Populate the test database with the root node and a user with id 1
+		 */
+		ExecutionEngine engine = new ExecutionEngine(mockDbService);
+		try (Transaction tx = mockDbService.beginTx()) {
+			ExecutionResult result = engine.execute("match (n:ROOT) return n");
+			Iterator<Node> nodes = result.columnAs("n");
+			Node rootNode = null;
+			if (!nodes.hasNext()) {
+				rootNode = mockDbService.createNode(Nodes.ROOT);
+				rootNode.setProperty("name", "Root node");
+				rootNode.setProperty("LAST_INSERTED_TRADITION_ID", "1000");
+			}
+
+			Node node = mockDbService.createNode(Nodes.USER);
+			node.setProperty("id", "1");
+			node.setProperty("isAdmin", "1");
+
+			rootNode.createRelationshipTo(node, ERelations.NORMAL);
+			
+			Node node2 = mockDbService.createNode(Nodes.USER);
+			node2.setProperty("id", "2");
+			node2.setProperty("isAdmin", "1");
+			
+			rootNode.createRelationshipTo(node2, ERelations.NORMAL);
+			
+			tx.success();
+		}
+		
+		/**
+		 * load a tradition to user 1
+		 */
+		try {
+			importResource.parseGraphML(filename, "1");
+		} catch (FileNotFoundException f) {
+			// this error should not occur
+			assertTrue(false);
+		}
+
+		/**
+		 * load a tradition to user 2
+		 */
+		try {
+			importResource.parseGraphML(filename, "2");
+		} catch (FileNotFoundException f) {
+			// this error should not occur
+			assertTrue(false);
+		}
+		
+		/**
+		 * gets the generated id of the inserted tradition
+		 */
+		try (Transaction tx = mockDbService.beginTx()) {
+			ExecutionResult result = engine.execute("match (u:USER)--(t:TRADITION) return t");
+			Iterator<Node> nodes = result.columnAs("t");
+			assertTrue(nodes.hasNext());
+			tradId = (String) nodes.next().getProperty("id");
+
+			tx.success();
+		}
+
+		/*
+		 * Now remove one user with all his traditions
+		 */
+		ClientResponse actualResponse = jerseyTest.resource().path("/user/1").delete(ClientResponse.class);
+		assertEquals(Response.Status.OK.getStatusCode(), actualResponse.getStatus());
+		
+//		/*
+//		 * Test if the 
+//		 */
+//		RelationshipModel rel = new RelationshipModel();
+//		rel.setSource("16");
+//		rel.setTarget("27");
+//		rel.setId("36");
+//		rel.setDe8("april");
+//		rel.setDe6("no");
+//		rel.setDe9("april");
+//		rel.setDe1("0");
+//		rel.setDe11("transposition");
+//		rel.setDe10("local");
+//
+//		List<RelationshipModel> relationships = jerseyTest.resource()
+//				.path("/tradition/relation/" + tradId + "/relationships")
+//				.get(new GenericType<List<RelationshipModel>>() {
+//				});
+//		RelationshipModel relLoaded = relationships.get(2);
+//
+//		assertEquals(rel.getSource(), relLoaded.getSource());
+//		assertEquals(rel.getTarget(), relLoaded.getTarget());
+//		assertEquals(rel.getId(), relLoaded.getId());
+//		assertEquals(rel.getDe8(), relLoaded.getDe8());
+//		assertEquals(rel.getDe6(), relLoaded.getDe6());
+//		assertEquals(rel.getDe9(), relLoaded.getDe9());
+//		assertEquals(rel.getDe1(), relLoaded.getDe1());
+//		assertEquals(rel.getDe11(), relLoaded.getDe11());
+//		assertEquals(rel.getDe10(), relLoaded.getDe10());
+
+		
+	}
+	
+	/**
+	 * Test user Removal with invalid userId
+	 */
 	
 	/**
 	 * Test if the traditions of a user are listed well
