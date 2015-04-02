6960e8d3e4ab4dae3543f5157fe1d73c4440f41e
Severin Zumbrunn <severin.zumbrunn@students.unibe.ch>
2015-04-02 16:22:33 +0200

    #DH-53 #time 4h #comment Added Dot to Neo4J parser. including test. (parser is not yet added to graphmlimporter)

---
+++ stemmarest/src/main/java/net/stemmaweb/services/DotToNeo4JParser.java
@@ -0,0 +1,171 @@
+package net.stemmaweb.services;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import net.stemmaweb.rest.IResource;
+import net.stemmaweb.rest.ERelations;
+import net.stemmaweb.rest.Nodes;
+
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.traversal.Uniqueness;
+
+import Exceptions.DataBaseException;
+
+/**
+ * This class provides methods for exporting Dot File from Neo4J
+ * @author sevi
+ * 
+ */
+public class DotToNeo4JParser
+{
+	private GraphDatabaseService db;
+	
+	String dot = "";
+	List<Node> nodes = new ArrayList<Node>();
+
+	public DotToNeo4JParser(GraphDatabaseService db){
+		this.db = db;
+	}
+
+	public void parseDot(String dot, String tradId)
+	{	
+		this.dot = dot;
+    	
+    	try (Transaction tx = db.beginTx()) 
+    	{
+    		while(nextObject(tradId));
+    			
+    		nodes.get(0).createRelationshipTo(nodes.get(1), ERelations.NORMAL);
+    		tx.success();
+    	}
+    	
+    	db.shutdown();
+	}
+	
+	/**
+	 * parses a dot into neo4j, object for object, returns false when no more objects exist
+	 * adds all nodes below tradId
+	 * @return
+	 */
+	private boolean nextObject(String tradId)
+	{
+		// check either graph or node/edge is next
+		int i;
+		if(((dot.indexOf('{')==-1)&&(dot.indexOf(';')!=-1))
+				|| (dot.indexOf(';')<dot.indexOf('{')))
+		{
+			i = dot.indexOf(';');
+			String tmp = dot.substring(0, i);
+			boolean undirected = false;
+			if((undirected = tmp.contains("--")) || tmp.contains("-&gt;") || tmp.contains("->"))
+			{
+				String[] splitted;
+				if(undirected)
+				{
+					splitted = tmp.split("--");
+					splitted[0] = splitted[0].replaceAll(" ", "");
+					splitted[1] = splitted[1].replaceAll(" ", "");
+					Node source = findNodeById(splitted[0]);
+					Node target = findNodeById(splitted[1]);
+					if(source!=null && target!=null)
+						source.createRelationshipTo(target, ERelations.NORMAL);
+				}
+				else
+				{
+					if(tmp.contains("->"))
+						splitted = tmp.split("->");
+					else
+						splitted = tmp.split("-&gt;");
+					splitted[0] = splitted[0].replaceAll(" ", "");
+					splitted[1] = splitted[1].replaceAll(" ", "");
+					Node source = findNodeById(splitted[0]);
+					Node target = findNodeById(splitted[1]);
+					if(source!=null && target!=null)
+						source.createRelationshipTo(target, ERelations.NORMAL);
+				}
+			}
+			else if(tmp.length()>0)
+			{
+				Node node = db.createNode(Nodes.WITNESS);
+				if(dot.indexOf('[')!=-1)
+				{
+					String[] splitted = tmp.split("\\[");
+					
+					splitted[0] = splitted[0].replaceAll(" ", "");
+					splitted[1] = splitted[1].replaceAll(" ", "");
+					
+					node.setProperty("id", splitted[0].trim());
+					
+					splitted[1] = splitted[1].replaceAll("\\[", "");
+					splitted[1] = splitted[1].replaceAll("\\]", "");
+					
+					// replace this with the use of the correct delimiter of properties
+					String[] sub = splitted[1].split(",");
+					for(String str : sub)
+					{
+						String[] arr = str.split("=");
+						node.setProperty(arr[0].trim(), arr[1].trim());
+					}
+					
+					nodes.add(node);
+				}
+			}
+			dot = dot.substring(i+2);
+		}
+		else if((i = dot.indexOf('{'))<dot.indexOf(';') && i>0)
+		{ // this holds something like ' graph "stemma" ' or 'digraph "stem23423" '
+			Node node = db.createNode(Nodes.STEMMA);
+			String tmp = dot.substring(0, i);
+			if(tmp.contains("digraph"))
+			{
+				node.setProperty("type", "digraph");
+			}
+			else if(tmp.contains("graph"))
+			{
+				node.setProperty("type", "graph");
+			}
+			String[] name = tmp.split("\"");
+			if(name.length==3)
+			{
+				node.setProperty("name", name[1]);
+			}
+			
+			Node trad = db.findNodesByLabelAndProperty(Nodes.TRADITION, "id", tradId).iterator().next();
+			if(trad!=null)
+				trad.createRelationshipTo(node, ERelations.NORMAL);
+			nodes.add(node);
+			dot = dot.substring(i+2);
+		}
+		else
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	private Node findNodeById(String id)
+	{
+		for(Node n : nodes)
+		{
+			if(n.hasProperty("id") && n.getProperty("id").equals(id))
+				return n;
+		}
+		return null;
+	}
+}
---
+++ stemmarest/src/test/java/net/stemmaweb/stemmaserver/unittests/DotImporterUnitTest.java
@@ -0,0 +1,212 @@
+package net.stemmaweb.stemmaserver.unittests;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import net.stemmaweb.rest.Nodes;
+import net.stemmaweb.rest.ERelations;
+import net.stemmaweb.services.DotToNeo4JParser;
+import net.stemmaweb.services.GraphMLToNeo4JParser;
+import net.stemmaweb.services.Neo4JToDotParser;
+import net.stemmaweb.stemmaserver.OSDetector;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.ResourceIterable;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import Exceptions.DataBaseException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DotImporterUnitTest {
+	private String tradId;
+	/*
+	 * Create a Mock object for the dbFactory.
+	 */
+	@Mock
+	protected GraphDatabaseFactory mockDbFactory = new GraphDatabaseFactory();
+
+	/*
+	 * Create a Spy object for dbService.
+	 */
+	@Spy
+	protected GraphDatabaseService mockDbService = new TestGraphDatabaseFactory().newImpermanentDatabase();
+
+	/*
+	 * The Resource under test. The mockDbFactory will be injected into this
+	 * resource.
+	 */
+	@InjectMocks
+	private GraphMLToNeo4JParser importResource;
+	
+	@InjectMocks
+	private DotToNeo4JParser parser;
+
+	@Before
+	public void setUp() throws Exception {
+
+		String filename = "";
+		if (OSDetector.isWin())
+			filename = "src\\TestXMLFiles\\testTradition.xml";
+		else
+			filename = "src/TestXMLFiles/testTradition.xml";
+
+		/*
+		 * Populate the test database with the root node and a user with id 1
+		 */
+		ExecutionEngine engine = new ExecutionEngine(mockDbService);
+		try (Transaction tx = mockDbService.beginTx()) {
+			ExecutionResult result = engine.execute("match (n:ROOT) return n");
+			Iterator<Node> nodes = result.columnAs("n");
+			Node rootNode = null;
+			if (!nodes.hasNext()) {
+				rootNode = mockDbService.createNode(Nodes.ROOT);
+				rootNode.setProperty("name", "Root node");
+				rootNode.setProperty("LAST_INSERTED_TRADITION_ID", "1000");
+			}
+
+			Node node = mockDbService.createNode(Nodes.USER);
+			node.setProperty("id", "1");
+			node.setProperty("isAdmin", "1");
+
+			rootNode.createRelationshipTo(node, ERelations.NORMAL);
+			tx.success();
+		}
+
+		/*
+		 * Manipulate the newEmbeddedDatabase method of the mockDbFactory to
+		 * return new TestGraphDatabaseFactory().newImpermanentDatabase()
+		 * instead of dbFactory.newEmbeddedDatabase("database");
+		 */
+		Mockito.when(mockDbFactory.newEmbeddedDatabase(Matchers.anyString())).thenReturn(mockDbService);
+
+		/*
+		 * Avoid the Databaseservice to shutdown. (Override the shutdown method
+		 * with nothing)
+		 */
+		Mockito.doNothing().when(mockDbService).shutdown();
+
+		/**
+		 * load a tradition to the test DB
+		 */
+		try {
+			importResource.parseGraphML(filename, "1");
+		} catch (FileNotFoundException f) {
+			// this error should not occur
+			assertTrue(false);
+		}
+		/**
+		 * gets the generated id of the inserted tradition
+		 */
+		try (Transaction tx = mockDbService.beginTx()) {
+			ExecutionResult result = engine.execute("match (u:USER)--(t:TRADITION) return t");
+			Iterator<Node> nodes = result.columnAs("t");
+			assertTrue(nodes.hasNext());
+			tradId = (String) nodes.next().getProperty("id");
+
+			tx.success();
+		}
+	}
+
+	@Test
+	public void testDotImport()
+	{
+		String dot = "digraph \"stemma\" {  O [ class=hypothetical ];  a [ class=hypothetical ]; "
+				+ " b [ class=hypothetical ];  c [ class=hypothetical ];  d [ class=hypothetical ];"
+				+ "  e [ class=hypothetical ];  f [ class=hypothetical ];  g [ class=hypothetical ];"
+				+ "  h [ class=hypothetical ];  An74 [ class=extant ];  Au318 [ class=extant ];"
+				+ "  Ba96 [ class=extant ];  Er16 [ class=extant ];  Go325 [ class=extant ];"
+				+ "  Gr314 [ class=extant ];  Kf133 [ class=extant ];  Krems185 [ class=extant ];"
+				+ "  Krems299 [ class=extant ];  Mu11475 [ class=extant ];  Mu22405 [ class=extant ];"
+				+ "  Mu28315 [ class=extant ];  MuU151 [ class=extant ];  Sg524 [ class=extant ];"
+				+ "  Wi3181 [ class=extant ];  Krems185 -> b;  Krems299 -> Mu22405;  "
+				+ "O -> a;  O -> b;  a -> Au318;  a -> Go325;  a -> Gr314;  a -> f;  a -> g;"
+				+ "  b -> Au318;  b -> Ba96;  b -> Go325;  b -> Gr314;  b -> Sg524;  b -> c;"
+				+ "  c -> An74;  c -> MuU151;  c -> d;  d -> Mu11475;  d -> e;  e -> Er16;"
+				+ "  e -> Mu28315;  f -> Krems299;  f -> h;  g -> Mu22405;  g -> Wi3181;"
+				+ "  h -> Kf133;  h -> Krems185;}";
+		
+		parser.parseDot(dot,tradId);
+		
+		dot = "graph \"Semstem 1402333041_0\" {  0 [ class=hypothetical ];  1 [ class=hypothetical ];"
+				+ "  10 [ class=hypothetical ];  2 [ class=hypothetical ];  3 [ class=hypothetical ];"
+				+ "  4 [ class=hypothetical ];  5 [ class=hypothetical ];  6 [ class=hypothetical ];"
+				+ "  7 [ class=hypothetical ];  8 [ class=hypothetical ];  9 [ class=hypothetical ];"
+				+ "  MuU151 [ class=hypothetical ];  An74 [ class=extant ];  Au318 [ class=extant ];"
+				+ "  Ba96 [ class=extant ];  Er16 [ class=extant ];  Go325 [ class=extant ];"
+				+ "  Gr314 [ class=extant ];  Kf133 [ class=extant ];  Kr185 [ class=extant ];"
+				+ "  Kr299 [ class=extant ];  Mu11475 [ class=extant ];  Mu22405 [ class=extant ];"
+				+ "  Mu28315 [ class=extant ];  Sg524 [ class=extant ];  Wi3818 [ class=extant ];"
+				+ "  10 -- Ba96;  10 -- Sg524;  1 -- 10;  1 -- 2;  2 -- 3;  2 -- Mu11475;  4 -- 3;"
+				+ "  5 -- 4;  6 -- 5;  7 -- 6;  7 -- 8;  7 -- Go325;  7 -- Wi3818;  8 -- Mu22405;"
+				+ "  9 -- 5;  9 -- Au318;  9 -- Kr185;  An74 -- 4;  Er16 -- 3;  Gr314 -- 8;"
+				+ "  Kf133 -- 6;  Kr299 -- 6;  Mu28315 -- 3;  MuU151 -- 0;  MuU151 -- 1;}";
+		
+		parser.parseDot(dot,tradId);
+		
+		try(Transaction tx = mockDbService.beginTx())
+		{
+			Node stemmaNode = mockDbService.findNodesByLabelAndProperty(Nodes.STEMMA, "name", "stemma").iterator().next();
+		
+			assert(stemmaNode!=null);
+			
+			Node firstNode = mockDbService.findNodesByLabelAndProperty(Nodes.WITNESS, "id", "0").iterator().next();
+			
+			assert(firstNode!=null);
+			
+			Node nodeB = mockDbService.findNodesByLabelAndProperty(Nodes.WITNESS, "id", "b").iterator().next();
+			
+			assert(nodeB!=null);
+			
+			boolean checker = false;
+			for(Relationship rel : firstNode.getRelationships(Direction.OUTGOING))
+			{
+				if(rel.getEndNode().equals(nodeB))
+					checker = true;
+			}
+			assert(checker==true);
+			
+			stemmaNode = mockDbService.findNodesByLabelAndProperty(Nodes.STEMMA, "name", "Semstem 1402333041_0").iterator().next();
+			
+			assert(stemmaNode!=null);
+			
+			Iterator<Node> nodes = mockDbService.findNodesByLabelAndProperty(Nodes.WITNESS, "id", "0").iterator();
+			nodes.next();
+			assert(nodes.next()!=null);
+		}
+		catch(Exception e)
+		{
+			assert(false);
+		}
+		
+	}
+	
+	
+	@After
+	public void destroyTestDatabase()
+	{
+	    mockDbService.shutdown();
+	    // destroy the test database
+	}
+}
