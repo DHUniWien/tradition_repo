e81d9cd0284678539352ed8ac3348961e20e0069
Jakob Schaerer <jakob.schaerer@students.unibe.ch>
2015-04-02 20:06:52 +0200

    #DH-106 #time 5m #comment simplified ApplicationConfig

--- stemmarest/src/main/java/net/stemmaweb/rest/ApplicationConfig.java
+++ stemmarest/src/main/java/net/stemmaweb/rest/ApplicationConfig.java
@@ -1,25 +1,21 @@
 package net.stemmaweb.rest;
 
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.core.Application;
 
 import net.stemmaweb.services.DatabaseService;
 
-import org.neo4j.cypher.javacompat.ExecutionResult;
-import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 
 public class ApplicationConfig extends Application {
 	
 	public static final String DB_PATH = "database"; // this is the local path to StemmaServer/database
-	static boolean init = false;
+		
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> s = new HashSet<Class<?>>();
@@ -28,21 +24,19 @@ public class ApplicationConfig extends Application {
         s.add(User.class);
         s.add(Tradition.class);
         s.add(Relation.class);
-        if(!init)
-        	initializeApp();
         
         return s;
     }
     
+    @PostConstruct
     public void initializeApp()
-    {
-    	init = true;
-    	
+    {	
     	GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
-    	GraphDatabaseService db= dbFactory.newEmbeddedDatabase(DB_PATH);
+    	GraphDatabaseService db = dbFactory.newEmbeddedDatabase(DB_PATH);
     	
-    	DatabaseService dbService = new DatabaseService(db);
-    	dbService.createRootNode();
+    	DatabaseService.createRootNode(db);
+    	
+    	db.shutdown();
     }
 
 }
