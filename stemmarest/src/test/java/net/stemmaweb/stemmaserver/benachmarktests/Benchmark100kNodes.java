dad91dfbb8e829e4f75c9318384483cd2ee5aa88
Jakob Schaerer <jakob.schaerer@students.unibe.ch>
2015-04-26 01:08:28 +0200

    #DH-109 #time 3h #comment added extensive benchmark tests

--- stemmarest/src/test/java/net/stemmaweb/stemmaserver/benachmarktests/Benchmark100kNodes.java
+++ stemmarest/src/test/java/net/stemmaweb/stemmaserver/benachmarktests/Benchmark100kNodes.java
@@ -1,12 +1,23 @@
 package net.stemmaweb.stemmaserver.benachmarktests;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Iterator;
+
+import net.stemmaweb.model.ReturnIdModel;
+import net.stemmaweb.stemmaserver.OSDetector;
 
 import org.junit.BeforeClass;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import com.carrotsearch.junitbenchmarks.annotation.AxisRange;
@@ -26,19 +37,49 @@ public class Benchmark100kNodes extends BenachmarkTests {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		
+		
 		/*
 		 * Fill the Testbench with a nice graph 9 users 2 traditions 5 witnesses with degree 10
 		 */
-		initDatabase();
-	}
-
-	public static void initDatabase() {
 		RandomGraphGenerator rgg = new RandomGraphGenerator();
 		GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
 		GraphDatabaseService db = dbFactory.newEmbeddedDatabase("database");
 		
 		rgg.role(db, 10, 10, 10, 100);
+		
+
 		db.shutdown();
+		
+		if (OSDetector.isWin())
+			filename = "src\\TestXMLFiles\\ReadingstestTradition.xml";
+		else
+			filename = "src/TestXMLFiles/ReadingstestTradition.xml";
+		
+		try {
+			tradId = importResource.parseGraphML(filename, "1").getEntity().toString().replace("{\"tradId\":", "").replace("}", "");
+		} catch (FileNotFoundException f) {
+			// this error should not occur
+			assertTrue(false);
+		}
+		
+		db = dbFactory.newEmbeddedDatabase("database");
+		ExecutionEngine engine = new ExecutionEngine(db);
+		ExecutionResult result = engine.execute("match (w:WORD {dn15:'showers'}) return w");
+		Iterator<Node> nodes = result.columnAs("w");
+		duplicateReadingNodeId = nodes.next().getId();
+
+		result = engine.execute("match (w:WORD {dn15:'the root'}) return w");
+		nodes = result.columnAs("w");
+		theRoot = nodes.next().getId();
+		
+		result = engine.execute("match (w:WORD {dn15:'unto me'}) return w");
+		nodes = result.columnAs("w");
+		untoMe = nodes.next().getId();
+		
+		db.shutdown();
+		
 	}
 	
 }
