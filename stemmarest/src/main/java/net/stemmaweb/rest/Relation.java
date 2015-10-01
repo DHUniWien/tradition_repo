666d7f0dd5f5019c6d075e8e7c4986e5f338c256
tla <tla@mit.edu>
2015-10-01 20:51:55 +0200

    Demonstrate the existing bugs that arise setting transposition relationships; fix a logic bug in wouldProduceCrossRelationship along the way

--- stemmarest/src/main/java/net/stemmaweb/rest/Relation.java
+++ stemmarest/src/main/java/net/stemmaweb/rest/Relation.java
@@ -140,8 +140,8 @@ public class Relation implements IResource {
             secondDirection = Direction.INCOMING;
         }
 
-        int depth = (int) (Long.parseLong(firstReading.getProperty("rank").toString())
-                - (Long.parseLong( secondReading.getProperty("rank").toString()))) + 1;
+        int depth = Math.abs((int) (Long.parseLong(firstReading.getProperty("rank").toString())
+                - (Long.parseLong( secondReading.getProperty("rank").toString())))) + 1;
 
         for (Node firstReadingNextNode : getNextNodes(firstReading, firstDirection, depth)) {
             for (Relationship rel : firstReadingNextNode.getRelationships(ERelations.RELATED)) {
