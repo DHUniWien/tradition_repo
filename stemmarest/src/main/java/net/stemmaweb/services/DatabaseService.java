45f1a16be4441d7b9cbc4341e309cdf6e44c4b87
Sascha Kaufmann <sascha.kaufmann@germ.unibe.ch>
2016-05-11 12:36:35 +0200

    now getStartNode() allows section-nodes, too
    - now getStartNode() allows section-nodes, too
    - new function getEndNode(); just to complete getStartNode()
    - new function getSectionNodes(); returns a tradition's section-node(s) in sorted order

--- stemmarest/src/main/java/net/stemmaweb/services/DatabaseService.java
+++ stemmarest/src/main/java/net/stemmaweb/services/DatabaseService.java
@@ -6,6 +6,8 @@ import java.util.Iterator;
 import net.stemmaweb.rest.ERelations;
 import net.stemmaweb.rest.Nodes;
 import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.traversal.Evaluators;
+import org.neo4j.graphdb.traversal.Uniqueness;
 
 /**
  * Helper methods for the database
@@ -15,19 +17,103 @@ import org.neo4j.graphdb.*;
 public class DatabaseService {
     /**
      *
-     * @param tradId
+     * @param nodeId
      * @param db
      *            the GraphDatabaseService where the tradition is stored
      * @return
      */
-    public static Node getStartNode(String tradId, GraphDatabaseService db) {
+    public static Node getStartNode(String nodeId, GraphDatabaseService db) {
+        Node startNode = null;
+        Node currentNode = getTraditionNode(nodeId, db);
+        if (currentNode == null) {
+            currentNode = getSectionNode(nodeId, db);
+        }
+        if (currentNode != null) {
+            ArrayList<Node> snList = getRelated(currentNode, ERelations.COLLATION);
+            try (Transaction tx = db.beginTx()) {
+                if (snList.size() == 0 && currentNode.hasLabel(Nodes.TRADITION)) {
+                    ArrayList<Node> sectionNodes = getSectionNodes(nodeId, db);
+                    for (Node iterNode : sectionNodes) {
+                        startNode = getStartNode(String.valueOf(iterNode.getId()), db);
+                        if (startNode != null) {
+                            break;
+                        }
+                    }
+                } else if (snList.size() == 1) {
+                    startNode = snList.get(0);
+                }
+                tx.success();
+            }
+        }
+        return startNode;
+    }
+
+    /**
+     *
+     * @param nodeId
+     * @param db
+     *            the GraphDatabaseService where the tradition is stored
+     * @return
+     */
+    public static Node getEndNode(String nodeId, GraphDatabaseService db) {
+        Node endNode = null;
+        Node currentNode = getTraditionNode(nodeId, db);
+        if (currentNode == null) {
+            currentNode = getSectionNode(nodeId, db);
+        }
+        if (currentNode != null) {
+            ArrayList<Node> snList = getRelated(currentNode, ERelations.HAS_END);
+            try (Transaction tx = db.beginTx()) {
+                if (snList.size() == 0 && currentNode.hasLabel(Nodes.TRADITION)) {
+                    ArrayList<Node> sectionNodes = getSectionNodes(nodeId, db);
+                    Node tmpEndNode;
+                    for (Node iterNode : sectionNodes) {
+                        tmpEndNode = getEndNode(String.valueOf(iterNode.getId()), db);
+                        if (tmpEndNode != null) {
+                            endNode = tmpEndNode;
+                        }
+                    }
+                } else if (snList.size() == 1) {
+                    endNode = snList.get(0);
+                }
+                tx.success();
+            }
+        }
+        return endNode;
+    }
+
+    /**
+     *
+     * @param tradId
+     * @param db        the GraphDatabaseService where the tradition is stored
+     * @return
+     */
+    public static ArrayList<Node> getSectionNodes(String tradId, GraphDatabaseService db) {
         Node tradition = getTraditionNode(tradId, db);
         if (tradition == null)
             return null;
-        ArrayList<Node> snList = getRelated(tradition, ERelations.COLLATION);
-        if (snList.size() != 1)
-            return null;
-        return snList.get(0);
+        ArrayList<Node> sectionNodes = new ArrayList<>();
+        ArrayList<Node> sections = DatabaseService.getRelated(tradition, ERelations.PART);
+        int size = sections.size();
+        try (Transaction tx = db.beginTx()) {
+            for(Node n: sections) {
+                if (!n.getRelationships(Direction.INCOMING, ERelations.NEXT)
+                        .iterator()
+                        .hasNext()) {
+                    db.traversalDescription()
+                            .depthFirst()
+                            .relationships(ERelations.NEXT, Direction.OUTGOING)
+                            .evaluator(Evaluators.toDepth(size))
+                            .uniqueness(Uniqueness.NODE_GLOBAL)
+                            .traverse(n)
+                            .nodes()
+                            .forEach(r -> sectionNodes.add(r));
+                    break;
+                }
+            }
+            tx.success();
+        }
+        return sectionNodes;
     }
 
     /**
@@ -45,6 +131,23 @@ public class DatabaseService {
         }
         return tradition;
     }
+
+    /**
+     *
+     * @param sectId
+     * @param db
+     *            the GraphDatabaseService where the tradition is stored
+     * @return
+     */
+    public static Node getSectionNode(String sectId, GraphDatabaseService db) {
+        Node section;
+        try (Transaction tx = db.beginTx()) {
+            section = db.findNode(Nodes.SECTION, "id", sectId);
+            tx.success();
+        }
+        return section;
+    }
+
     /**
      *
      * @param db: the GraphDatabaseService where the Database should be entered
