27cdd02416f10ec765f08ed03379f0d319c1da42
oftl <github@rockandroll.at>
2017-07-11 12:15:38 +0200

    enable inline volumes; minor refactoring

--- ci/resources/deploy.sh
+++ ci/resources/deploy.sh
@@ -1,54 +1,65 @@
 #!/bin/sh
 
 CONFIG_FILE=$1
-SSH_KEYS_DIR=ssh-keys
 
 . ${CONFIG_FILE}
-
-/bin/chmod 600 ${SSH_KEYS_DIR}/storage/ci-id_rsa
 
 SSH="/usr/bin/ssh \
     -o StrictHostKeyChecking=no \
     -i ${SSH_KEYS_DIR}/storage/ci-id_rsa \
     ci@${DEPLOY_HOST}"
 
-INSTANCE_RUNNING=`$SSH sudo /usr/bin/docker ps --format "{{.Names}}" | grep -e "stemmarest-${INSTANCE}"`
-if [[ "${INSTANCE_RUNNING}" ]] ; then
-    echo "stop running instance <${NAME}> ..."
-    $SSH sudo /usr/bin/docker stop stemmarest-${INSTANCE}
+INSTANCE_RUNNING=`$SSH sudo /usr/bin/docker ps --format "{{.Names}}" | grep -e "${INSTANCE_NAME}"`
+INSTANCE_RUNNING=`$SSH sudo /usr/bin/docker ps --format "{{.Names}}" | grep -e "${INSTANCE_NAME}"`
+INSTANCE_RUNNING=`$SSH sudo /usr/bin/docker ps --format "{{.Names}}" | grep -e "${INSTANCE_NAME}"`
+if [ "${INSTANCE_RUNNING}" ] ; then
+    echo "stop running instance <${INSTANCE_NAME}> ..."
+    $SSH sudo /usr/bin/docker stop ${INSTANCE_NAME}
 fi
 
-INSTANCE_EXISTS=`$SSH sudo /usr/bin/docker ps -a --format "{{.Names}}" | grep -e "stemmarest-${INSTANCE}"`
-if [[ "${INSTANCE_EXISTS}" ]] ; then
-    echo "remove instance <${NAME}> ..."
-    $SSH sudo /usr/bin/docker rm stemmarest-${INSTANCE}
-fi
-
-if [[ ! "${VOLUME}" ]] ; then
-    echo "prepare to use temporary volume"
-    VOLUME="temp_volume"
-fi
-
-VOLUME_EXISTS=`$SSH sudo /usr/bin/docker volume ls | awk "{print $2}" | grep -e "${VOLUME}"`
-if [[ ! "${VOLUME_EXISTS}" ]] ; then
-    echo "create missing volume <${VOLUME}> ..."
-    $SSH sudo /usr/bin/docker volume create --name ${VOLUME}
+INSTANCE_EXISTS=`$SSH sudo /usr/bin/docker ps -a --format "{{.Names}}" | grep -e "${INSTANCE_NAME}"`
+if [ "${INSTANCE_EXISTS}" ] && [ "${REMOVE_OLD_INSTANCE}" = "yes" ] ; then
+    echo "remove instance <${INSTANCE_NAME}> ..."
+    echo "please note, that only inline volumes will be removed with it"
+    $SSH sudo /usr/bin/docker rm ${INSTANCE_NAME}
 fi
 
 echo "pull latest of <${REGISTRY}/${IMAGE}:${TAG}>"
 $SSH sudo /usr/bin/docker pull ${REGISTRY}/${IMAGE}:${TAG}
 
-echo "create new container ..."
-$SSH sudo /usr/bin/docker create \
-  --name    stemmarest-${INSTANCE} \
-  --env     STEMMAREST_HOME=/var/lib/stemmarest/ \
-  --user    tomcat8 \
-  --publish ${HOST}:${HOST_PORT}:${CONTAINER_PORT} \
-  --volume  ${VOLUME}:/var/lib/stemmarest/ \
-  --memory  ${MEMORY} \
-  --cpu-period=100000 \
-  --cpu-quota=100000 \
-  --restart always \
-${REGISTRY}/${IMAGE}:${TAG}
+# sh:   -n ... True if the length of string is nonzero.
+# bash: -v ... True if the shell variable varname is set (has been assigned a value).
+if [ -n "${VOLUME}" ] ; then
+    VOLUME_EXISTS=`$SSH sudo /usr/bin/docker volume ls | awk "{print $2}" | grep -e "${VOLUME}"`
+    if [ ! "${VOLUME_EXISTS}" ] ; then
+        echo "create volume <${VOLUME}> ..."
+        $SSH sudo /usr/bin/docker volume create --name ${VOLUME}
+    fi
 
-$SSH sudo /usr/bin/docker start stemmarest-${INSTANCE}
+    echo "create new container with volume <${VOLUME}>..."
+    $SSH sudo /usr/bin/docker create \
+      --name    ${INSTANCE_NAME} \
+      --env     STEMMAREST_HOME=/var/lib/stemmarest/ \
+      --user    tomcat8 \
+      --publish ${HOST}:${HOST_PORT}:${CONTAINER_PORT} \
+      --volume  ${VOLUME}:/var/lib/stemmarest/ \
+      --memory  ${MEMORY} \
+      --cpu-period=100000 \
+      --cpu-quota=100000 \
+      --restart always \
+    ${REGISTRY}/${IMAGE}:${TAG}
+else
+    echo "create new container with inline volume ..."
+    $SSH sudo /usr/bin/docker create \
+      --name    ${INSTANCE_NAME} \
+      --env     STEMMAREST_HOME=/var/lib/stemmarest/ \
+      --user    tomcat8 \
+      --publish ${HOST}:${HOST_PORT}:${CONTAINER_PORT} \
+      --memory  ${MEMORY} \
+      --cpu-period=100000 \
+      --cpu-quota=100000 \
+      --restart always \
+    ${REGISTRY}/${IMAGE}:${TAG}
+fi
+
+$SSH sudo /usr/bin/docker start ${INSTANCE_NAME}
