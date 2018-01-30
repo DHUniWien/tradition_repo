0e605eee6b1a914dd952a99d3fd9b7f4a38fa9bd
Tara L Andrews <tla@mit.edu>
2018-01-30 17:31:41 +0100

    logical reordering for ease of comment / understanding

--- stemmarest/src/main/java/net/stemmaweb/rest/ERelations.java
+++ stemmarest/src/main/java/net/stemmaweb/rest/ERelations.java
@@ -10,18 +10,18 @@ import org.neo4j.graphdb.RelationshipType;
 public enum ERelations implements RelationshipType {
     // Directed types
     SYSTEMUSER,     // links the root node to the user(s)
-    SEQUENCE,       // the basic link between word sequences in a text
     OWNS_TRADITION, // between user and tradition
-    COLLATION,      // between tradition part and START node
-    LEMMA_TEXT,     // to indicate word sequence
-    HAS_END,        // between tradition part and END node
+    PART,           // links a tradition with its (section) parts
+    NEXT,           // link between sections as ordered in 1 or more witnesses
+    PUB_ORDER,      // link between sections to preserve a canonical order
+    COLLATION,      // between section part and START node
+    HAS_END,        // between section part and END node
+    SEQUENCE,       // the basic link between word sequences in a text
+    LEMMA_TEXT,     // to indicate canonical word sequence
     HAS_WITNESS,    // links text witnesses to the tradition
     HAS_STEMMA,     // this type is used to make a stemma tree (directed/undirected)
     HAS_ARCHETYPE,  // used to point from the stemma node to its archetype
     TRANSMITTED,    // links witnesses in the stemma to each other
-    PART,           // links a tradition with its (section) parts
-    NEXT,           // link between sections
-    PUB_ORDER,      // link between sections to preserve their publication order
 
     // Undirected types
     RELATED,        // this type is used to show relationships between readings (undirected)
